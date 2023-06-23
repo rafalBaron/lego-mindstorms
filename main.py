0c527f298532b018596552ca07205c23d6e91bce
Rafał Baron <rafal.IT@baron.hub.pl>
2023-06-23 16:38:59 +0200

    final

--- main.py
+++ main.py
@@ -7,25 +7,8 @@ from pybricks.tools import wait
 from pybricks.robotics import DriveBase
 import math
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
 ev3 = EV3Brick()
-#color_sensor = ColorSensor(Port.S4)
-#left_motor = Motor(Port.B)
-#right_motor = Motor(Port.C)
-#robot = DriveBase(left_motor, right_motor, wheel_diameter=29, axle_track=117)
-DRIVE_SPEED = 70
-
-PROPORTIONAL_GAIN = 1.8
-
-
-# Write your program here.
 ev3.speaker.beep()
-
 
 class robocik:
 
@@ -39,12 +22,12 @@ class robocik:
         self.right_motor = Motor(Port.C)
         self.nr = self.left_motor.angle()
         self.nl = self.right_motor.angle()
-        self.center_sensor = UltrasonicSensor(Port.S2)
-        self.left_sensor = InfraredSensor(Port.S1)
+        self.center_sensor = UltrasonicSensor(Port.S1)
+        self.left_sensor = InfraredSensor(Port.S3)
         self.right_sensor = InfraredSensor(Port.S4)
 
-    def droga(self):
-
+    
+    def update_position(self):
         self.nl_current = self.left_motor.angle()
         self.nr_current = self.right_motor.angle()
         self.delta_nl = self.nl_current - self.nl
@@ -53,48 +36,42 @@ class robocik:
         self.Dr = 2 * math.pi * self.r * (self.delta_nr/360)
         self.nr = self.nr_current
         self.nl =  self.nl_current
-
-        self.DC = (self.Dl + self.Dr)/2
-
-        return [self.DC, self.Dl, self.Dr]
         
-    def update_position(self):
-        self.x = self.x + self.droga()[0] * math.cos(self.position()[2])
-        self.y = self.y + self.droga()[0] * math.sin(self.position()[2])
-        self.fi = self.fi + ((self.droga()[2] - self.droga()[1])/self.L)
-
-    def position(self):
-        return [self.x,self.y,self.fi]
-    
+        self.DC = (self.Dl + self.Dr)/2
+        
+        self.x = self.x + self.DC * math.cos(self.fi)
+        self.y = self.y + self.DC * math.sin(self.fi)
+        self.fi = self.fi + ((self.Dr - self.Dl)/self.L)
+        
     def run(self, v, omega):
         self.v_right = v + omega
         self.v_left = v - omega
-        #ev3.screen.print((round(self.x),round(self.y),self.fi))
+        ev3.screen.print((round(self.x/10),round(self.y/10),self.fi))
         self.left_motor.run(self.v_left)
         self.right_motor.run(self.v_right)
 
     def unikaj(self):
         dist_L, dist_R, dist_C = self.distance_przeszkoda()
-        x_c = dist_C*math.cos(self.position()[2]) + 5*math.cos(self.position()[2]) + self.position()[0]
-        y_c = dist_C*math.sin(self.position()[2]) + 5*math.sin(self.position()[2]) + self.position()[1]
-        x_l = dist_L*(0.5*math.cos(self.position()[2])-math.sqrt(3/2)*math.sin(self.position()[2]))-8*math.sin(self.position()[2])+8*math.cos(self.position()[2])+self.position()[0]
-        y_l = dist_L*(0.5*math.sin(self.position()[2])+math.sqrt(3/2)*math.cos(self.position()[2]))+8*math.sin(self.position()[2])+8*math.cos(self.position()[2])+self.position()[1]
-        x_r = dist_R*(math.sqrt(3/2)*math.sin(self.position()[2])+0.5*math.cos(self.position()[2]))-8*math.sin(self.position()[2])+8*math.cos(self.position()[2])+self.position()[0]
-        y_r = dist_R*(0.5*math.sin(self.position()[2])-math.sqrt(3/2)*math.cos(self.position()[2]))+8*math.sin(self.position()[2])+8*math.cos(self.position()[2])+self.position()[1]
-        x_obst = (x_c - self.position()[0]) + (x_l - self.position()[0]) + (x_r - self.position()[0])
-        y_obst = (y_c - self.position()[1]) + (y_l - self.position()[1]) + (y_r - self.position()[1])
+        x_c = dist_C*math.cos(self.fi) + 5*math.cos(self.fi) + self.x
+        y_c = dist_C*math.sin(self.fi) + 5*math.sin(self.fi) + self.y
+        x_l = dist_L*(0.5*math.cos(self.fi)-math.sqrt(3/2)*math.sin(self.fi))-8*math.sin(self.fi)+8*math.cos(self.fi)+self.x
+        y_l = dist_L*(0.5*math.sin(self.fi)+math.sqrt(3/2)*math.cos(self.fi))+8*math.sin(self.fi)+8*math.cos(self.fi)+self.y
+        x_r = dist_R*(math.sqrt(3/2)*math.sin(self.fi)+0.5*math.cos(self.fi))-8*math.sin(self.fi)+8*math.cos(self.fi)+self.x
+        y_r = dist_R*(0.5*math.sin(self.fi)-math.sqrt(3/2)*math.cos(self.fi))+8*math.sin(self.fi)+8*math.cos(self.fi)+self.y
+        x_obst = (x_c - self.x) + (x_l - self.x) + (x_r - self.x)
+        y_obst = (y_c - self.y) + (y_l - self.y) + (y_r - self.y)
         return x_obst, y_obst
         
     def distance_przeszkoda(self):
-        dist_L = self.left_sensor.distance()*0.7
-        dist_R = self.right_sensor.distance()*0.7
-        dist_C = self.center_sensor.distance()/10
+        dist_L = self.left_sensor.distance()*7
+        dist_R = self.right_sensor.distance()*7
+        dist_C = self.center_sensor.distance()
 
         return dist_L, dist_R, dist_C
 
     def clear(self):
         dist_L, dist_R, dist_C = self.distance_przeszkoda()
-        if dist_L>30 and dist_R>30 and dist_C>50:
+        if dist_L>300 and dist_R>300 and dist_C>500:
             return True
         else:
             return False
@@ -103,27 +80,28 @@ class robocik:
         x_obst, y_obst = self.unikaj()
         dist_L, dist_R, dist_C = self.distance_przeszkoda()
         ksi = math.atan2(y_obst, x_obst)
-        e = ksi - self.position()[2]
+        e = ksi - self.fi
         Kp = 150
-        v = 100
-        if(dist_L>40 and dist_R>40 and dist_C>70):
+        v = 150
+        if(dist_L>300 and dist_R>300 and dist_C>500):
             Kp = 0
+            v = 300
         omega = Kp*e
         self.run(v, omega)
             
     def go_to_goal(self, goal):
         xg, yg = goal
-        while (math.sqrt((xg-self.position()[0])**2+(yg-self.position()[1])**2) > 100):
-            ksi = math.atan2(yg - self.position()[1], xg - self.position()[0])
-            e = ksi - self.position()[2]
-            Kp = 215
+        while (math.sqrt((xg-self.x)**2+(yg-self.y)**2) > 250):
+            ksi = math.atan2(yg - self.y, xg - self.x)
+            e = ksi - self.fi
+            Kp = 110  
             omega = Kp*e
-            v = 250
-            #if self.clear():
-            self.run(v,omega)
-            #else:
-            #self.omin()
-            print(round(self.position()[0]),round(self.position()[1]),self.position()[2])
+            v = 300
+            if self.clear():
+                self.run(v,omega)
+            else:
+                self.omin()
+            print(round(self.x/10),round(self.y/10),self.fi)
             #ev3.screen.print((round(self.x),round(self.y),self.fi))
             self.update_position()
         self.stop()
@@ -136,10 +114,10 @@ class robocik:
 while not Button.LEFT in ev3.buttons.pressed():
     pass
 
-waypoints = [[2000, 0], [1250, 1000], [1250, 2000]]
+#waypoints = [[2000, 0], [1250, 1000], [1250, 2000]]
+waypoints = [[0,-3500]]
 robot = robocik()
 for index, goal in enumerate(waypoints):
-    ev3.screen.print(goal, index)
     robot.go_to_goal(goal)
 
 
